c9c756f63a9acbc3392f5564e05c55cb02bcdfe2
javieroc <ferroxido@gmail.com>
2018-02-25 23:10:28 -0300

    Add fake data

--- src/components/Spot/Spot.js
+++ src/components/Spot/Spot.js
@@ -3,26 +3,37 @@ import ReactStars from 'react-stars';
 import './Spot.css';
 
 class Spot extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
+    const {
+      name,
+      address,
+      phone,
+      rating,
+      photo,
+    } = this.props;
     return (
       <div className="spot">
         <div>
           <img
-            src="./assets/field.jpg"
+            src={photo}
             alt=""
             width={360}
             height={240}
           />
         </div>
-        <div className="spot-name">Spot Name</div>
+        <div className="spot-name">{name}</div>
         <div className="spot-info">
-          <div>Address: calle falsa 123s</div>
-          <div>Phone: 123123123</div>
+          <div>Address: {address}</div>
+          <div>Phone: {phone}</div>
           <div className="spot-rating">
             <ReactStars
               count={5}
               size={24}
-              value={3}
+              value={rating}
               color2={'#ffd700'}
             />
           </div>
--- src/components/Spot/Spotlist.js
+++ src/components/Spot/Spotlist.js
@@ -2,16 +2,89 @@ import React, { Component } from 'react';
 import Spot from './Spot';
 import './Spotlist.css';
 
+const spotlist = [
+  {
+    name: 'Central Norte',
+    description: 'This is a description test',
+    address: 'Fake street 123',
+    phone: '987654321',
+    location: {
+      lat: '-24.7893779',
+      lng: '-65.4075302',
+    },
+    photo: 'https://t3.ftcdn.net/jpg/01/87/76/70/240_F_187767015_Y123csJ7E7dqaQnQDSJCntlLTbXxsZdH.jpg',
+    rating: 3,
+  },
+  {
+    name: 'Central Norte',
+    description: 'This is a description test',
+    address: 'Fake street 123',
+    phone: '987654321',
+    location: {
+      lat: '-24.7893779',
+      lng: '-65.4075302',
+    },
+    photo: 'https://t3.ftcdn.net/jpg/01/87/76/70/240_F_187767015_Y123csJ7E7dqaQnQDSJCntlLTbXxsZdH.jpg',
+    rating: 3,
+  },
+  {
+    name: 'Central Norte',
+    description: 'This is a description test',
+    address: 'Fake street 123',
+    phone: '987654321',
+    location: {
+      lat: '-24.7893779',
+      lng: '-65.4075302',
+    },
+    photo: 'https://t3.ftcdn.net/jpg/01/87/76/70/240_F_187767015_Y123csJ7E7dqaQnQDSJCntlLTbXxsZdH.jpg',
+    rating: 3,
+  },
+  {
+    name: 'Central Norte',
+    description: 'This is a description test',
+    address: 'Fake street 123',
+    phone: '987654321',
+    location: {
+      lat: '-24.7893779',
+      lng: '-65.4075302',
+    },
+    photo: 'https://t3.ftcdn.net/jpg/01/87/76/70/240_F_187767015_Y123csJ7E7dqaQnQDSJCntlLTbXxsZdH.jpg',
+    rating: 3,
+  },
+  {
+    name: 'Central Norte',
+    description: 'This is a description test',
+    address: 'Fake street 123',
+    phone: '987654321',
+    location: {
+      lat: '-24.7893779',
+      lng: '-65.4075302',
+    },
+    photo: 'https://t3.ftcdn.net/jpg/01/87/76/70/240_F_187767015_Y123csJ7E7dqaQnQDSJCntlLTbXxsZdH.jpg',
+    rating: 3,
+  },
+  {
+    name: 'Central Norte',
+    description: 'This is a description test',
+    address: 'Fake street 123',
+    phone: '987654321',
+    location: {
+      lat: '-24.7893779',
+      lng: '-65.4075302',
+    },
+    photo: 'https://t3.ftcdn.net/jpg/01/87/76/70/240_F_187767015_Y123csJ7E7dqaQnQDSJCntlLTbXxsZdH.jpg',
+    rating: 3,
+  },
+];
+
 class Spotlist extends Component {
   render() {
+    const list = spotlist.map((spot) => {
+      return <Spot {...spot} />
+    });
     return (
       <div className="spotlist">
-        <Spot />
-        <Spot />
-        <Spot />
-        <Spot />
-        <Spot />
-        <Spot />
+        {list}
       </div>
     );
   }
